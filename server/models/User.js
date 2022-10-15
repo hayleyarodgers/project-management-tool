a49da9e61967149d839bc355226f372b2d6b106e
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-15 17:10:03 +1100

    Write User model

---
+++ server/models/User.js
@@ -0,0 +1,78 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// Schema to create User model
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			// Use regex to validate email
+			match: [
+				/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+				"Please use a valid email address.",
+			],
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		// Array of ids of projects created by a user
+		projects: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "project",
+			},
+		],
+		// Array of ids of team members created by a user
+		teamMembers: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "teamMember",
+			},
+		],
+	},
+	// Allow use of virtuals below
+	{
+		toJSON: {
+			virtuals: true,
+		},
+	}
+);
+
+// Hash user's password
+userSchema.pre("save", async function (next) {
+	if (this.isNew || this.isModified("password")) {
+		const saltRounds = 10;
+		this.password = await bcrypt.hash(this.password, saltRounds);
+	}
+
+	next();
+});
+
+// Custom method to compare and validate password when user logs in
+userSchema.methods.isCorrectPassword = async function (password) {
+	return bcrypt.compare(password, this.password);
+};
+
+// Create a virtual property "projectCount" that gets the number of projects a user has
+userSchema.virtual("projectCount").get(function () {
+	return this.projects.length;
+});
+
+// Create a virtual property "teamCount" that gets the number of people in a users's team
+userSchema.virtual("teamCount").get(function () {
+	return this.teamMembers.length;
+});
+
+// Initialise User model
+const User = model("user", userSchema);
+
+module.exports = User;
