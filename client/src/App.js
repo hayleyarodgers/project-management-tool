a4f6143600e1f7520edd3f0386bdec0c86763697
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-15 16:28:59 +1100

    Add basic file structure

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Import components
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+	uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+	// Get the authentication token from local storage if it exists
+	const token = localStorage.getItem("id_token");
+	// Return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		},
+	};
+});
+
+// Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
+
+function App() {
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<>
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={SearchBooks} />
+						<Route exact path="/saved" component={SavedBooks} />
+						<Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+					</Switch>
+				</>
+			</Router>
+		</ApolloProvider>
+	);
+}
+
+export default App;
---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require("./User");
+const Project = require("./Project");
+const TeamMember = require("./TeamMember");
+
+module.exports = { User, Project, TeamMember };
