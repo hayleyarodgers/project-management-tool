b20290b7a73fc05f0048b4dead02b12b2d8c6ec2
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-15 20:54:59 +1100

    Write controllers for project manager user

---
+++ server/controllers/userProjectManagerController.js
@@ -0,0 +1,54 @@
+// Import project manager user model
+const { UserProjectManager } = require("../models");
+// Import sign token function from auth
+const { signToken } = require("../utils/auth");
+
+module.exports = {
+	// Get a project manager user by their id
+	async getSingleUserProjectManager({ params }, res) {
+		const user = await UserProjectManager.findOne({ _id: params.userId });
+
+		if (!user) {
+			return res.status(400).json({ message: "No user with that id." });
+		}
+
+		res.json(user);
+	},
+
+	// Upon sign up, create a project manager user, sign a token and send it back to client/src/components/SignUpForm.js
+	async createUser({ body }, res) {
+		const user = await UserProjectManager.create(body);
+
+		if (!user) {
+			return res
+				.status(400)
+				.json({ message: "Unable to create new project manager user." });
+		}
+
+		const token = signToken(user);
+		res.json({ token, user });
+	},
+
+	// Upon log in, log in the project manager user based on username, sign a token and send it back to client/src/components/LoginForm.js)
+	async login({ body }, res) {
+		const user = await UserProjectManager.findOne({ username: body.username });
+
+		if (!user) {
+			return res
+				.status(400)
+				.json({
+					message: "Unable to find project manager user with this username.",
+				});
+		}
+
+		// Check password
+		const correctPw = await UserProjectManager.isCorrectPassword(body.password);
+
+		if (!correctPw) {
+			return res.status(400).json({ message: "Wrong password." });
+		}
+
+		const token = signToken(user);
+		res.json({ token, user });
+	},
+};
---
+++ server/utils/auth.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+
+// Set token secret and expiration date
+const secret = process.env.TOKEN_SECRET;
+const expiration = "2h";
+
+module.exports = {
+	// Function for our authenticated routes
+	authMiddleware: function ({ req }) {
+		// Allows token to be sent via req.body, req.query or headers
+		let token = req.body.token || req.query.token || req.headers.authorization;
+
+		// Split the token string into an array and return the actual token
+		// ["Bearer", "<tokenvalue>"]
+		if (req.headers.authorization) {
+			token = token.split(" ").pop().trim();
+		}
+
+		if (!token) {
+			return res.status(400).json({ message: "You have no token!" });
+		}
+
+		// If the token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+		try {
+			const { data } = jwt.verify(token, secret, { maxAge: expiration });
+			req.user = data;
+		} catch {
+			console.log("Invalid token.");
+			return res.status(400).json({ message: "Invalid token." });
+		}
+
+		// Send to next endpoint.
+		next();
+	},
+	signToken: function ({ username, email, _id }) {
+		const payload = { username, email, _id };
+
+		return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+	},
+};
