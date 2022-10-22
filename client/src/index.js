59b737b1a9e36a125951c77df2ecfcc406a4d6de
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-22 13:31:22 +1100

    Link global styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./styles/reset.css";
+import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
