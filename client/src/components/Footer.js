1ea019ce5d1575932f2561ce47a3693ad8ae1ff8
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-22 15:58:23 +1100

    Update background

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,6 +4,9 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto text-muted p-4">
+      <br />
+      <br />
+      <br />
       <div className="container text-center mb-5">
         <h4>&copy; {new Date().getFullYear()} - Hayley Rodgers</h4>
         <p>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <main>
+    <main className="home">
       <h2>Home</h2>
     </main>
   );
