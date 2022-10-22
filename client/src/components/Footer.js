69d092512c8e9fe65e58697777815f6698658536
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-22 13:29:47 +1100

    Adjust footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,17 +1,14 @@
 import React from "react";
+import "../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto text-dark p-4">
+    <footer className="w-100 mt-auto text-muted p-4">
       <div className="container text-center mb-5">
         <h4>&copy; {new Date().getFullYear()} - Hayley Rodgers</h4>
         <p>
           Made using Mongoose, Express.js, React.js, Node.js and a RESTful API
-          on Gadigal Land.
-        </p>
-        <p>
-          {" "}
-          To see the source code, click{" "}
+          on Gadigal Land. To see the source code, click{" "}
           <a
             href="https://github.com/hayleyarodgers/project-management-tool"
             target="_blank"
