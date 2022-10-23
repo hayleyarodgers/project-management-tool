bd8e02fea61e4fc974d0b0a006c3c7ea5351591a
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-24 10:32:38 +1100

    Style no match page

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,16 +1,30 @@
 import React from "react";
+
+// Import `useLocation()` to retrieve the current `location` object data from React Router
 import { useLocation } from "react-router-dom";
 
+// Import Link component for all internal application hyperlinks
+import { Link } from "react-router-dom";
+
+// Import error image
+import error from "../assets/error.jpg";
+
 const NotFound = () => {
-  // Retrieve the current `location` object data from React Router
   let location = useLocation();
 
   return (
-    <div>
-      <h2>
-        No match for <code>{location.pathname}.</code>
-      </h2>
-    </div>
+    <main className="d-flex justify-content-between align-items-top">
+      <div>
+        <h2>Oops!</h2>
+        <p>
+          There doesn't appear to be a page for <b>{location.pathname}</b>.
+        </p>
+        <Link className="btn" variant="success" to={`/`}>
+          Go back to home page
+        </Link>
+      </div>
+      <img src={error} alt="Error" style={{ width: "50%", margin: "auto" }} />
+    </main>
   );
 };
 
